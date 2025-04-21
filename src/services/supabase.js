353196088c93324ed79466cbc35d46b6d2e66df9
Calcifer077 <maheshnashier14@gmail.com>
2025-04-21 22:15:28 +0530

    Updated some files

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-// import { SUPABASE_URL } from "../utils/constants";
-// import { SUPABASE_KEY } from "../utils/constants";
 const url = import.meta.env.VITE_SUPABASE_URL;
 const api = import.meta.env.VITE_SUPABASE_KEY;
 
